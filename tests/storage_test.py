89afbd1ab422a39ba4fa5e18ecc4f8e7c95e9b0f
Nick Wolf <WhiteWolf1776@pm.me>
2024-06-05 16:35:00 -0500

    additional basic tests

--- tests/storage_test.py
+++ tests/storage_test.py
@@ -11,10 +11,26 @@ with open("tests/config/dev_storage.cfg", 'r') as f:
 	storage_path = f.readline().strip()
 	global storage_extension
 	storage_extension = f.readline().strip()
+	global storage_sub_path1
+	storage_sub_path1 = f.readline().strip()
+	global storage_sub_path2
+	storage_sub_path2 = f.readline().strip()
 
 def testStorage():
 	std_client_raw = storage.adls_client(storage_account, storage_container)
 	paths = storage.get_files_by_extension(
-				std_client_raw, storage_path, storage_extension)
+				std_client_raw, storage_sub_path2, storage_extension)
 	print(paths)
 	assert len(paths) > 10
+
+
+def testAvro():
+	std_client_raw = storage.adls_client(storage_account, storage_container)
+	paths = storage.get_files_by_extension(
+					std_client_raw, storage_sub_path1,
+					storage_extension)
+	assert len(paths) > 1
+
+
+def testAvroToDelta():
+	assert True
